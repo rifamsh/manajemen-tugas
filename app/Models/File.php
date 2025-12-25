ce50c88d1a03e945f87f700a02bc418f7b46bca0
Rifamsh <most.crush31@gmail.com>
2025-12-25 12:45:44 +0700

    ketinggalan

--- app/Models/File.php
+++ app/Models/File.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class File extends Model
 {
-    //
+    protected $fillable = [
+        'task_id',
+        'file_name',
+        'file_path',
+    ];
+
+    public function task()
+    {
+        return $this->belongsTo(Task::class);
+    }
 }
